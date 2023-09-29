4bf387c54853b611b40c4daa68b8bf90f3007670
williamvn <wnoguerol@commercehub.com>
2023-09-29 12:51:02 +0200

    Add external prompt implementation

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Alert, Button, Platform, Text, View } from 'react-native'
 import { globalStyles } from '../theme/AppTheme'
+import prompt from 'react-native-prompt-android';
 
 export const AlertScreen = () => {
     const showAlert = () => Alert.alert('Alert Title', "This is an alert message, did you understand?", [
@@ -8,18 +9,35 @@ export const AlertScreen = () => {
         { text: 'Ok', onPress: () => console.log('Ok Pressed'), style: 'default' },
     ], { cancelable: true });
 
-    const showPrompt = () => Alert.prompt(
+    const showNativeIOsPrompt = () => Alert.prompt(
         "IOS Prompt",
-        "some message",
+        "Enter Password",
         (value: string) => console.log("info:", value),
         'login-password',
         'Default Value'
-    )
+    );
+
+    const showPrompt = () => prompt( //external package, not recommended but hey here it is
+        'Enter password',
+        'Enter your password to claim your $1.5B in lottery winnings',
+        [
+            { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+            { text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password) },
+        ],
+        {
+            type: 'secure-text',
+            cancelable: false,
+            defaultValue: 'test',
+            placeholder: 'placeholder'
+        });
 
     return (
         <View style={globalStyles.centerContainer}>
-            <Button title='Alert 1 ' onPress={showAlert} />
-            {Platform.OS === 'ios' && <Button title='IOS Prompt ' onPress={showPrompt} />}
+            <Button title='Alert 1' onPress={showAlert} />
+            <View style={{margin: 5}}></View>
+            <Button title='Prompt' onPress={showPrompt} />
+            <View style={{margin: 5}}></View>
+            {Platform.OS === 'ios' && <Button title='Native IOS Prompt ' onPress={showNativeIOsPrompt} />}
         </View>
     );
 }
